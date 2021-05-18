a0e92b88e3db1a061357be579ab63f323587d981
c0delite <budianto.andri92@gmail.com>
2021-05-18 09:32:00 +0700

    update navbar

--- src/components/admin/AdminNavbar.vue
+++ src/components/admin/AdminNavbar.vue
@@ -22,18 +22,20 @@
                                 </path>
                             </svg>
                         </button>
-                        <span class="w-1 h-8 rounded-lg bg-gray-200">
-                        </span>
-                        <a href="#" class="block relative">
-                            <img alt="profil" src="/images/person/1.jpg" class="mx-auto object-cover rounded-full h-10 w-10 "/>
-                        </a>
-                        <button class="flex items-center text-gray-500 dark:text-white text-md">
-                            {{loggedInUser.username}}
-                            <svg width="20" height="20" class="ml-2 text-gray-400" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z">
-                                </path>
-                            </svg>
-                        </button>
+                        <div v-if="isAuthenticated">
+                            <div @click="toggle" class="relative">
+                                <button class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+                                <span>{{loggedInUser.username}}</span>
+                                <svg :class="[isOpen ? 'rotate-0':'rotate-180']" fill="currentColor" viewBox="0 0 20 20"  class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                </button>
+                                <div :class="[isOpen ? 'block' : 'hidden']" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
+                                <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
+                                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Profile</a>
+                                    <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" @click="logout">Log out</a>
+                                </div>
+                                </div>
+                            </div>    
+                        </div>
                     </div>
                 </div>
             </header>
@@ -42,8 +44,21 @@
 <script>
 import {mapGetters} from "vuex";
 export default {
+    data() {
+    return {
+      isOpen: false
+    };
+  },
 computed: {
     ...mapGetters(["isAuthenticated", "loggedInUser"]),
+    },
+    methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+    async logout(){
+      await this.$auth.logout()
+    }
     },
 }
 </script>
